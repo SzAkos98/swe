9a7162dc672452a146496152903c8d7c440c3619
Ákos Szabó <asdlol0001@gmail.com>
2019-05-11 19:02:36 +0200

    Added Main Menu with functions and added Game Start Menu with button functions, but without nickname save. Added Game StackPane.

---
+++ src/main/java/game/Main.java
@@ -0,0 +1,148 @@
+package game;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage window;
+    Scene scene1, scene2, scene3;
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("ProjectSWE");
+        Rectangle bg = new Rectangle(1280, 720);
+        bg.setStroke(Color.DARKCYAN);
+        bg.setFill(Color.ANTIQUEWHITE);
+
+        StackPane layout1 = new StackPane();
+        layout1.getChildren().add(bg);
+        initMainMenu(layout1);
+        window.setResizable(false);
+        window.setScene(scene1);
+        window.show();
+    }
+
+    protected void initMainMenu(Pane menuLayout) {
+        //background and font size fixed to 72
+        Rectangle bg = new Rectangle(1280, 720);
+        bg.setStroke(Color.DARKCYAN);
+        bg.setFill(Color.ANTIQUEWHITE);
+        Font font = Font.font(72);
+
+        //lapel
+        AnchorPane menu = new AnchorPane();
+        menu.setPrefSize(1280, 100);
+        Label mLabel = new Label("Welcome to the ProjectSWE!");
+        mLabel.setFont(font);
+        mLabel.setLayoutX(110);
+        mLabel.setLayoutY(10);
+        menu.getChildren().add(mLabel);
+
+        //start button
+        Button btnStart = new Button("Start");
+        btnStart.setFont(font);
+        btnStart.setOnAction(actionEvent -> window.setScene(scene2));
+
+        //exit button
+        Button btnExit = new Button("Exit");
+        btnExit.setFont(font);
+        btnExit.setOnAction(actionEvent -> System.exit(0));
+
+        //display
+        VBox btns = new VBox(50, btnStart, btnExit);
+        btns.setAlignment(Pos.CENTER);
+
+
+        menuLayout.getChildren().addAll(menu, btns);
+        scene1 = new Scene(menuLayout, 1280, 720);
+        StackPane asd = new StackPane();
+        asd.getChildren().add(bg);
+        gameStart(asd);
+    }
+
+    protected void gameStart(Pane gameStart) {
+        Rectangle bg = new Rectangle(1280, 720);
+        bg.setStroke(Color.DARKCYAN);
+        bg.setFill(Color.ANTIQUEWHITE);
+
+        String nick = "Enter your nickname";
+        Font font = Font.font(25);
+        AnchorPane nicks = new AnchorPane();
+        nicks.setPrefSize(1280, 720);
+
+        //player 1 nickname
+        TextField player1 = new TextField();
+        player1.setPromptText(nick);
+        player1.setFont(font);
+        player1.setMaxHeight(60);
+        player1.setMaxWidth(300);
+        Label plyr1 = new Label("Player 1");
+        plyr1.setFont(font);
+        VBox p1 = new VBox(50, plyr1, player1);
+        p1.setAlignment(Pos.CENTER);
+        p1.setLayoutX(200);
+        p1.setLayoutY(150);
+
+        //player 2 nickname
+        TextField player2 = new TextField();
+        player2.setPromptText(nick);
+        player2.setFont(font);
+        player2.setMaxHeight(60);
+        player2.setMaxWidth(300);
+        Label plyr2 = new Label("Player 2");
+        plyr2.setFont(font);
+        VBox p2 = new VBox(50, plyr2, player2);
+        p2.setAlignment(Pos.CENTER);
+        p2.setLayoutX(780);
+        p2.setLayoutY(150);
+
+        //game starter button
+        Button btnStartGame = new Button("Start Game");
+        btnStartGame.setFont(Font.font(30));
+        btnStartGame.setAlignment(Pos.CENTER);
+        btnStartGame.setOnAction(actionEvent -> window.setScene(scene3));
+
+        //back to menu button
+        Button btnBack = new Button("Back");
+        btnBack.setFont(Font.font(25));
+        btnBack.setAlignment(Pos.CENTER);
+        btnBack.setOnAction(actionEvent -> window.setScene(scene1));
+
+        VBox btns = new VBox(30, btnStartGame, btnBack);
+        btns.setAlignment(Pos.BOTTOM_CENTER);
+
+        nicks.getChildren().addAll(p1, p2);
+
+        gameStart.getChildren().addAll(nicks, btns);
+        scene2 = new Scene(gameStart, 1280, 720);
+        StackPane asd = new StackPane();
+        asd.getChildren().add(bg);
+        game(asd);
+
+    }
+
+    protected void game(Pane gm) {
+        StackPane gmPane = new StackPane();
+        gm.getChildren().addAll(gmPane);
+        scene3 = new Scene(gm, 1280, 720);
+    }
+}
